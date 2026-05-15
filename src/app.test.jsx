5179cfe36c83537cc5ad9caff62bb43e5639c5c9
kiriromark.prog <kiriromark@gmail.com>
2026-05-15 12:22:11 +0300

    Please work

---
+++ src/app.test.jsx
@@ -0,0 +1,5 @@
+
+
+test('math works', () => {
+    expect(1 + 1).toBe(2)
+    })
--- src/card.jsx
+++ src/card.jsx
@@ -1,10 +1,11 @@
-function Card({ name, price, image, onAdd }) {
+function Card({id, name, price, image, onAdd }) {
     return (
         <div className="card">
+           
             <img src={image} alt={name} />
             <h2>{name}</h2>
             <p className="price">${price}</p>
-            <button className="add" onClick={() => onAdd({ name, price, image })}>Add to Cart</button>
+            <button className="add" data-testid={`product-${id}`} onClick={() => onAdd({id, name, price, image })}>Add to Cart</button>
         </div>
     );
 }
